94ddeea0ac4625143c7f92e4135b818fc869ac8f
Kravantokh <mamaia7777@gmail.com>
2022-09-22 22:20:35 +0300

    Some changes to cmake.

--- compile.sh
+++ compile.sh
@@ -1,8 +1,8 @@
 #gcc -Wall -std=c89 tests/fancy.c src/*.c dependencies/benutils_unicode/src/*.c -I dependencies/benutils_unicode/include -I include -o test.o
 
 #cmake -S . -B build -GNinja -DCMAKE_BUILD_TYPE=RelWithDebInfo
-cmake -S . -B build -GNinja -DCMAKE_BUILD_TYPE=Debug
-#cmake -S . -B build -GNinja 
+#cmake -S . -B build -GNinja -DCMAKE_BUILD_TYPE=Debug
+cmake -S . -B build -GNinja -DBUILD_EXAMPLES=ON
 cd build
 ninja
 cd ..
